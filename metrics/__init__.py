a10e756b2de72e46c2e06e7db7dd417b5a38763a
ErickNgumo <erickmwangi678@gmail.com>
2025-11-20 21:44:42 +0300

    added access paths to __nit__.py for easier imports

---
+++ metrics/__init__.py
@@ -0,0 +1,23 @@
+from .winloss.winloss import (
+    win_rate,
+    loss_rate,
+    breakeven_rate,
+    average_win,
+    average_loss,
+    expectancy,
+    profit_factor,
+    longest_win_streak,
+    longest_loss_streak,
+    streak_distribution,
+    win_loss_ratio,
+    summary
+)
+
+from .equitycurve.equitycurve import (
+    to_equity_curve,
+)
+
+from .tradelog.tradelog import (
+    TradeLog
+)
+
